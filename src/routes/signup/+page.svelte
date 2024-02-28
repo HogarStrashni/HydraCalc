9a43ef963d05f2f57a6b6ad138dd610ff7f8b476
Đorđe Matić <djmatic@agfbl.org>
2024-02-28 09:43:00 +0100

    add login and signup forms

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,6 +1,29 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+</script>
+
 <div class="flex flex-col h-screen">
 	<a href="/" class="ml-auto max-w-fit m-4 border-2 px-5 py-3 text-lg rounded-lg">Back</a>
 	<div class="flex-1 flex items-center justify-center">
-		<h1>SIGNUP ROUTE</h1>
+		<form class="w-96 space-y-8" method="post" use:enhance>
+			<div class="flex flex-col">
+				<label for="email">Email</label>
+				<input type="text" id="email" name="email" class="border-2 rounded px-2 py-1" required />
+			</div>
+			<div class="flex flex-col">
+				<label for="password">Password</label>
+				<input
+					type="text"
+					id="password"
+					name="password"
+					class="border-2 rounded px-2 py-1"
+					required
+				/>
+			</div>
+			<br />
+			<div class="flex justify-end">
+				<button type="submit" class="border-2 px-5 py-1.5 rounded-lg">Sign Up</button>
+			</div>
+		</form>
 	</div>
 </div>
