e371b2fe56f0c882e9ea4bb625b81e02535f51a9
Đorđe Matić <djmatic@agfbl.org>
2024-03-01 10:46:20 +0100

    add input-field custom component

---
+++ src/lib/components/custom-ui/input-field.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { cn } from '@/utils';
+
+	import type { HTMLInputAttributes } from 'svelte/elements';
+
+	import { Input } from '$lib/components/ui/input';
+	import { Label } from '$lib/components/ui/label';
+
+	export let type: HTMLInputAttributes['type'] = 'text';
+	export let name: HTMLInputAttributes['name'] = undefined;
+	export let value: HTMLInputAttributes['value'] = undefined;
+	export let disabled: HTMLInputAttributes['disabled'] = undefined;
+	export let placeholder: HTMLInputAttributes['placeholder'] = undefined;
+
+	let className: HTMLInputAttributes['class'] = undefined;
+	export { className as class };
+
+	export let label: string | undefined = undefined;
+	export let error: string | undefined = undefined;
+</script>
+
+<div class={cn('space-y-1', className)}>
+	<Label for={name}>{label}</Label>
+	<Input
+		{type}
+		{name}
+		id={name}
+		{value}
+		{disabled}
+		{placeholder}
+		class={error ? 'border-destructive' : null}
+		aria-invalid={error ? 'true' : undefined}
+		on:input
+	/>
+	<p class="h-4 text-xs text-destructive">{error ?? ''}</p>
+</div>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,32 +1,20 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import InputField from '@/components/custom-ui/input-field.svelte';
 	import { Button } from '@/components/ui/button';
 </script>
 
 <div class="flex flex-col h-full">
 	<a href="/" class="ml-auto max-w-fit m-4 border-2 px-5 py-3 text-lg rounded-lg">Back</a>
-	<div class="flex-1 flex flex-col items-center justify-center space-y-12">
-		<form class="w-96 space-y-8" method="post" use:enhance>
-			<div class="flex flex-col">
-				<label for="email">Email</label>
-				<input type="text" id="email" name="email" class="border-2 rounded px-2 py-1" required />
-			</div>
-			<div class="flex flex-col">
-				<label for="password">Password</label>
-				<input
-					type="password"
-					id="password"
-					name="password"
-					class="border-2 rounded px-2 py-1"
-					required
-				/>
-			</div>
-			<br />
-			<div class="flex justify-end">
+	<div class="flex-1 flex flex-col items-center justify-center">
+		<form class="w-96" method="post" use:enhance>
+			<InputField label="Email" name="email" />
+			<InputField label="Password" name="password" class="mt-4" />
+			<div class="flex justify-end mt-8">
 				<Button type="submit" class="w-full">Login</Button>
 			</div>
 		</form>
-		<div class="flex justify-end">
+		<div class="flex justify-end mt-12">
 			<a href="/api/login/google" class="w-96 text-center py-2 rounded-md text-white bg-gray-700">
 				SIGN IN WITH GOOGLE
 			</a>
