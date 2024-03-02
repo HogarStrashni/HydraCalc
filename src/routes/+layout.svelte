39d4c35109e524f9ec5ae3dea27300408fc86e17
Đorđe Matić <djmatic@agfbl.org>
2024-03-02 12:17:10 +0100

    implement horizontal progress bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,10 @@
 	import '../app.css';
 	import { enhance } from '$app/forms';
 	import { fade } from 'svelte/transition';
-	import { page } from '$app/stores';
+	import { page, navigating } from '$app/stores';
 
 	import { Button } from '@/components/ui/button';
+	import ProgressBar from '@/components/custom-ui/progress-bar.svelte';
 	import Typography from '@/components/custom-ui/typography.svelte';
 
 	import { ArrowLeft, LogIn, LogOut } from 'lucide-svelte';
@@ -14,32 +15,39 @@
 	$: ({ user } = data);
 	$: isLoginRoute = ['/signin', '/signup'].includes($page.url.pathname);
 
-	let isSigningOut = false;
+	// delay LoadingStage for 200 ms
+	let isLoadingProgressBar = false;
+
+	$: if (!!$navigating === true) {
+		setTimeout(() => {
+			isLoadingProgressBar = !!$navigating;
+		}, 200);
+	} else {
+		isLoadingProgressBar = false;
+	}
 </script>
 
-<header class="flex items-center justify-end space-x-4 border-b pb-4">
-	{#if isLoginRoute}
-		<Button href="/" variant="outline" class="ml-auto max-w-fit" icon={ArrowLeft}>Back</Button>
-	{:else if user?.id}
-		<Typography as="h2" class="mr-auto">{user.email}</Typography>
-		<form method="post" use:enhance>
-			<Button
-				type="submit"
-				variant="outline"
-				icon={LogOut}
-				bind:isLoading={isSigningOut}
-				on:click={() => (isSigningOut = true)}
-			>
-				Sign Out
-			</Button>
-		</form>
-	{:else}
-		<Button href="/signin" variant="outline" icon={LogIn}>Sign In</Button>
-	{/if}
-</header>
+{#if isLoadingProgressBar}
+	<ProgressBar />
+{/if}
 
-{#key $page.url.pathname}
-	<main in:fade={{ duration: 300 }} class="h-full flex-1">
-		<slot />
-	</main>
-{/key}
+<div class="mx-auto flex h-[100dvh] max-w-7xl flex-col px-4 py-4 sm:px-8">
+	<header class="flex items-center justify-end space-x-4 border-b pb-4">
+		{#if isLoginRoute}
+			<Button href="/" variant="outline" class="ml-auto max-w-fit" icon={ArrowLeft}>Back</Button>
+		{:else if user?.id}
+			<Typography as="h2" class="mr-auto">{user.email}</Typography>
+			<form method="post" use:enhance>
+				<Button type="submit" variant="outline" icon={LogOut}>Sign Out</Button>
+			</form>
+		{:else}
+			<Button href="/signin" variant="outline" icon={LogIn}>Sign In</Button>
+		{/if}
+	</header>
+
+	{#key $page.url.pathname}
+		<main in:fade={{ duration: 300 }} class="h-full flex-1">
+			<slot />
+		</main>
+	{/key}
+</div>
