227850d1aa69eb3c77e0b4bc76e2b56f12dac2bf
Đorđe Matić <djmatic@agfbl.org>
2024-03-04 11:39:56 +0100

    implement signout api route

---
+++ src/routes/api/signout/+server.ts
@@ -0,0 +1,19 @@
+import { lucia } from '$lib/server/auth.js';
+import { redirect } from '@sveltejs/kit';
+
+export const GET = async ({ cookies, locals: { session } }) => {
+	if (!session) {
+		return new Response(null, {
+			status: 400
+		});
+	}
+
+	await lucia.invalidateSession(session.id);
+	const sessionCookie = lucia.createBlankSessionCookie();
+	cookies.set(sessionCookie.name, sessionCookie.value, {
+		path: '.',
+		...sessionCookie.attributes
+	});
+
+	redirect(302, '/');
+};
