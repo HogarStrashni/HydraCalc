5a97f662a6edb004e5f46dfc28bef8b0be0ce23c
Đorđe Matić <djmatic@agfbl.org>
2024-03-05 09:51:05 +0100

    update resend-utils and zod schema

--- src/lib/zod-schema.ts
+++ src/lib/zod-schema.ts
@@ -4,11 +4,19 @@ export const signinFormSchema = z.object({
 	email: z.string().email({ message: 'Email must be valid' }),
 	password: z.string().min(6, { message: 'Password must be at least 6 characters' })
 });
-
 export type SigninForm = z.infer<typeof signinFormSchema>;
 
-export const validationCodeSchema = z.object({
+export const validationCodeFormSchema = z.object({
 	code: z.string().min(6, { message: 'Enter valid code' })
 });
+export type ValidationCode = z.infer<typeof validationCodeFormSchema>;
 
-export type ValidationCode = z.infer<typeof validationCodeSchema>;
+export const resetPasswordFormSchema = z.object({
+	email: z.string().email({ message: 'Email must be valid' })
+});
+export type ResetPasswordForm = z.infer<typeof resetPasswordFormSchema>;
+
+export const newPasswordFormSchema = z.object({
+	password: z.string().min(6, { message: 'Password must be at least 6 characters' })
+});
+export type NewPasswordForm = z.infer<typeof newPasswordFormSchema>;
